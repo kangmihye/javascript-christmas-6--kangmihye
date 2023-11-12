69da8de16323f48b02d72e60a0aaf5fae767456f
MiHye <57440192+kangmihye@users.noreply.github.com>
2023-11-13 02:01:27 +0900

    refactor: 로직 변경
    - 디저트 또는 메인메뉴가 포함된 개수 세는 함수를 하나로 합침
    
    - 음료만 주문했는지 판별하는 함수 로직 변경

--- src/model/MeneList.js
+++ src/model/MeneList.js
@@ -7,21 +7,24 @@ class MenuList {
 		this.#menuList = menuList;
 	}
 
-	generateDessertList() {
+	countMenu(menuObj) {
 		let cnt = 0;
-		for (let menu in DESSERT) {
-			if (this.menuList.has(DESSERT[menu])) cnt += Number(this.menuList.get(DESSERT[menu]));
-			return cnt;
+		for (let menu in menuObj) {
+			if (this.menuList.has(menuObj[menu])) cnt += Number(this.menuList.get(menuObj[menu]));
 		}
+		return cnt;
 	}
-
-	generateMainDishList() {
-		const main = ["티본스테이크", "바비큐립", "해산물파스타", "크리스마스파스타"];
-		return [...this.menuList.keys()].filter((menu) => main.includes(menu)); // 갯수만 출력해도...?
-	}
+	// generateMainDishList() {
+	// const main = ["티본스테이크", "바비큐립", "해산물파스타", "크리스마스파스타"];
+	// return [...this.menuList.keys()].filter((menu) => main.includes(menu)); // 갯수만 출력해도...?
+	// }
 
 	isOnlyDrink() {
-		const drinks = ["제로콜라", "레드와인", "샴페인"];
-		return [...this.menuList.keys()].every((menu) => drinks.includes(menu));
+		for (let menu in DRINK) {
+			if (![...this.menuList.keys()].includes(DRINK[menu])) return false;
+		}
+		return true;
+		// const drinks = ["제로콜라", "레드와인", "샴페인"];
+		// return [...this.menuList.keys()].every((menu) => drinks.includes(menu));
 	}
 }
