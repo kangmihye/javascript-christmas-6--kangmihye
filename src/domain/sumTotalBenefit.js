3f27cbe248633d646600ca66f86a5a42a50d0a81
MiHye <57440192+kangmihye@users.noreply.github.com>
2023-11-13 22:40:50 +0900

    refactor: 총혜택 금액 함수 수정
    - 할인 후 예상 결제 금액을 구하기 위해 총혜택 금액에 증정품 가격을 포함
    sumTotal .benefit()
    - 증정품 가격을 빼고 할인 가격만 더한 함수 구현 sumTotal .discount()

--- src/domain/sumTotalBenefit.js
+++
@@ -1,9 +0,0 @@
-const sumTotalBenefit = (appliedEvents) => {
-	if (appliedEvents.canApplyEvents) {
-		const { xmas, star, week } = appliedEvents;
-		return xmas + star + week[1];
-	}
-	return 0;
-};
-
-export default sumTotalBenefit;
