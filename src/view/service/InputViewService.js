784521ff9090ecebd42beaf9342b33a43f3d22e6
MiHye <57440192+kangmihye@users.noreply.github.com>
2023-11-15 05:21:15 +0900

    refator: 유효성 검사를 위한 로직 수정
    - 입력 받은 메뉴를 유효성 검사하기 위해
    데이터 전처리 과정을 변수로 선언
    - 유효성 검사 validteInputMenu

--- src/view/service/InputViewService.js
+++ src/view/service/InputViewService.js
@@ -1,15 +1,16 @@
+import { validteInputMenu } from "../service/validateInput.js";
 const InputViewService = {
-	generateMenuList: (menusAndCnt) => {
-		const splitedMenus = menusAndCnt
-			.replaceAll(" ", "")
-			.split(",")
-			.map((menu) => menu.split("-"));
-		return getMap(splitedMenus);
+	generateMenuList: (inputMenu) => {
+		const splitedMenus = inputMenu.replaceAll(" ", "").split(",");
+		const splitedMenuAndCnt = splitedMenus.map((menu) => menu.split("-"));
+		validteInputMenu(inputMenu, splitedMenus, splitedMenuAndCnt);
+		return InputViewService.getMap(splitedMenuAndCnt);
 	},
 	getMap: (splitedMenus) => {
 		const menuList = new Map();
 		splitedMenus.forEach(([menu, cnt]) => {
-			menuList.has(menu) ? menuList.set(menu, menuList.get(menu) + Number(cnt)) : menuList.set(menu, Number(cnt));
+			menuList.set(menu, Number(cnt));
+			// menuList.has(menu) ? menuList.set(menu, menuList.get(menu) + Number(cnt)) : menuList.set(menu, Number(cnt));
 		});
 		return menuList;
 	},
