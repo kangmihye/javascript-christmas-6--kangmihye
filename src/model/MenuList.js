7a5fa41d8c9e485f2cbfa421032dee685ce7ac38
MiHye <57440192+kangmihye@users.noreply.github.com>
2023-11-15 02:55:32 +0900

    refactor: 메뉴 리스트의 메뉴 개수를 숫자로 계산
    - 메뉴 개수를 숫자로 저장한 것에 대한 로직 수정

--- src/model/MenuList.js
+++ src/model/MenuList.js
@@ -14,7 +14,7 @@ class MenuList {
 	sumTotalPrice() {
 		let total = 0;
 		this.#menuList.forEach((cnt, menu) => {
-			total += PRICE[menu] * Number(cnt);
+			total += PRICE[menu] * cnt;
 		});
 		return total;
 	}
@@ -22,14 +22,10 @@ class MenuList {
 	countMenu(menuObj) {
 		let cnt = 0;
 		for (let menu in menuObj) {
-			if (this.#menuList.has(menuObj[menu])) cnt += Number(this.#menuList.get(menuObj[menu]));
+			if (this.#menuList.has(menuObj[menu])) cnt += this.#menuList.get(menuObj[menu]);
 		}
 		return cnt;
 	}
-	// generateMainDishList() {
-	// const main = ["티본스테이크", "바비큐립", "해산물파스타", "크리스마스파스타"];
-	// return [...this.menuList.keys()].filter((menu) => main.includes(menu)); // 갯수만 출력해도...?
-	// }
 
 	//예외처리로빼자
 	isOnlyDrink() {
@@ -37,8 +33,6 @@ class MenuList {
 			if (![...this.#menuList.keys()].includes(DRINK[menu])) return false;
 		}
 		return true;
-		// const drinks = ["제로콜라", "레드와인", "샴페인"];
-		// return [...this.menuList.keys()].every((menu) => drinks.includes(menu));
 	}
 }
 
