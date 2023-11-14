04cb6bca1c1f246333d2edc174ef9b9a0949e1b0
MiHye <57440192+kangmihye@users.noreply.github.com>
2023-11-15 05:51:38 +0900

    feat: 입력받은 메뉴 데이터 전처리 함수 구현
    - 입력 받은 메뉴들의 유효성 검사의 매개변수를 1개만 받기 위해 데이터 전처리만 해주는 함수를 선언 preprocessData

--- src/view/service/InputViewService.js
+++ src/view/service/InputViewService.js
@@ -1,18 +1,20 @@
 import { validteInputMenu } from "../service/validateInput.js";
 const InputViewService = {
-	generateMenuList: (inputMenu) => {
+	preprocessData: (inputMenu) => {
 		const splitedMenus = inputMenu.replaceAll(" ", "").split(",");
 		const splitedMenuAndCnt = splitedMenus.map((menu) => menu.split("-"));
-		validteInputMenu(inputMenu, splitedMenus, splitedMenuAndCnt);
-		return InputViewService.getMap(splitedMenuAndCnt);
+		return [splitedMenus, splitedMenuAndCnt];
 	},
-	getMap: (splitedMenus) => {
+	getMap: (splitedMenuAndCnt) => {
 		const menuList = new Map();
-		splitedMenus.forEach(([menu, cnt]) => {
+		splitedMenuAndCnt.forEach(([menu, cnt]) => {
 			menuList.set(menu, Number(cnt));
-			// menuList.has(menu) ? menuList.set(menu, menuList.get(menu) + Number(cnt)) : menuList.set(menu, Number(cnt));
 		});
 		return menuList;
+	},
+	generateMenuList: (inputMenu) => {
+		const [, splitedMenuAndCnt] = InputViewService.preprocessData(inputMenu);
+		return InputViewService.getMap(splitedMenuAndCnt);
 	},
 };
 
