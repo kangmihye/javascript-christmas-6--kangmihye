517553dc4b507a6e35b68940829541b0042d1aec
MiHye <57440192+kangmihye@users.noreply.github.com>
2023-11-15 23:26:09 +0900

    refactor: 주석제거

--- src/utils/condition.js
+++ src/utils/condition.js
@@ -1,27 +1,20 @@
 import { APPETIZER, MAIN, DESSERT, DRINK } from "../utils/Constants.js";
 export const isInvalidNumber = (input) => {
-	// 실수를 입력할 경우,문자를 입력할 경우 true
 	input = Number(input.replaceAll(" ", ""));
 	return !Number.isInteger(input) || Number.isNaN(input);
 };
 
 export const isInvalidRange = (input) => {
-	// 1~31 이외일 경우 true
 	input = Number(input.replaceAll(" ", ""));
 	return input < 1 || input > 31;
 };
 
 export const hasComma = (inputMenu, splitedMenus) => {
-	//메뉴를 ,로 구분하지 않을 경우
-	//스테이크-2.아이스크림-2,초코-3
-	return inputMenu.match(/-/g).length !== splitedMenus.length; //같지 않으면 true = 잘못됨
+	return inputMenu.match(/-/g).length !== splitedMenus.length;
 };
 
 export const hasHyphenNum = (splitedMenus) => {
-	// 각 메뉴가 -3 형식이 아닐 경우
-	//  3
-	// ,아아스크림-2
-	return !splitedMenus.every((splitedMenu) => ~splitedMenu.search(/-\d+/g)); //true일 경우 올바름(가짐) false일 경우 안가져 -> 예외true
+	return !splitedMenus.every((splitedMenu) => ~splitedMenu.search(/-\d+/g));
 };
 
 export const hasRestaurantMenuList = (splitedMenuAndCnt) => {
@@ -33,14 +26,14 @@ export const hasRestaurantMenuList = (splitedMenuAndCnt) => {
 		menuListValue.push(restaurantMenuList[menu]);
 	}
 
-	return !splitedMenuAndCnt.every(([menu]) => menuListValue.includes(menu)); //false일 경우 없어 없으면 에러 true
+	return !splitedMenuAndCnt.every(([menu]) => menuListValue.includes(menu));
 };
 
 export const isInvalidMenuCount = (splitedMenuAndCnt) => {
 	return splitedMenuAndCnt.some(([, cnt]) => {
 		if (!Number(cnt)) return true;
 		isInvalidNumber(cnt);
-	}); // 실수 문자일 경우 true 하나라도 true면 true
+	});
 };
 
 export const isDuplicate = (splitedMenuAndCnt) => {
@@ -48,11 +41,10 @@ export const isDuplicate = (splitedMenuAndCnt) => {
 	splitedMenuAndCnt.forEach(([menu]) => {
 		set.add(menu);
 	});
-	return set.size !== splitedMenuAndCnt.length; // 같지 않으면 중복 true
+	return set.size !== splitedMenuAndCnt.length;
 };
 
 export const isOnlyDrink = (splitedMenuAndCnt) => {
-	//음료만 주문할 경우 true
 	const menus = splitedMenuAndCnt.map(([menu]) => menu);
 	const drink = [];
 	for (let menu in DRINK) {
@@ -62,6 +54,5 @@ export const isOnlyDrink = (splitedMenuAndCnt) => {
 };
 
 export const hasExceededTwenty = (splitedMenuAndCnt) => {
-	//20개가 넘었을 경우 true
 	if (splitedMenuAndCnt.reduce((sum, [, cnt]) => sum + Number(cnt), 0) > 20) return true;
 };
