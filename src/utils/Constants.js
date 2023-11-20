94dd8eabf04189f1cc2a7c13729228b94e9ee31e
MiHye <57440192+kangmihye@users.noreply.github.com>
2023-11-20 20:51:30 +0900

    feat: 메뉴와 메뉴 가격 상수 변수 선언
    - 각각 메뉴의 종류별로 메뉴의 이름과 메뉴 가격에 대한 상수 변수 선언

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -9,3 +9,42 @@ export const OUTPUT_VIEW = Object.freeze({
 	orderedMenu: "<주문 메뉴>",
 	totalPrice: "<할인 전 총주문 금액>",
 });
+
+export const APPETIZER = Object.freeze({
+	soup: "양송이수프",
+	tapas: "타파스",
+	salad: "시저샐러드",
+});
+
+export const MAIN = Object.freeze({
+	steak: "티본스테이크",
+	rip: "바비큐립",
+	pasta: "해산물파스타",
+	XmasPasta: "크리스마스파스타",
+});
+
+export const DESSERT = Object.freeze({
+	cake: "초코케이크",
+	icecream: "아이스크림",
+});
+
+export const DRINK = Object.freeze({
+	coke: "제로콜라",
+	wine: "레드와인",
+	champagne: "샴페인",
+});
+
+export const PRICE = Object.freeze({
+	[APPETIZER.soup]: 6000,
+	[APPETIZER.tapas]: 5500,
+	[APPETIZER.salad]: 8000,
+	[MAIN.steak]: 55000,
+	[MAIN.rip]: 54000,
+	[MAIN.pasta]: 35000,
+	[MAIN.XmasPasta]: 25000,
+	[DESSERT.cake]: 15000,
+	[DESSERT.icecream]: 5000,
+	[DRINK.coke]: 3000,
+	[DRINK.wine]: 60000,
+	[DRINK.champagne]: 25000,
+});
