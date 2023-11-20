df70cbe9d40136d726502422aea24fdc80df49fd
MiHye <57440192+kangmihye@users.noreply.github.com>
2023-11-20 21:03:50 +0900

    feat: 총 주문 금액을 구하는 기능 구현
    - 메뉴 리스트 객체 내에 총 주문 금액을 구하는 기능 구현
    - 상수 객체에서 메뉴 이름으로 해당 금액을 가져오는 로직
    sumTotalPrice()

--- src/model/MenuList.js
+++ src/model/MenuList.js
@@ -6,6 +6,20 @@ class MenuList {
 	constructor(menuList) {
 		this.#menuList = new Map(menuList);
 	}
+
+	sumTotalPrice() {
+		let totalPrice = 0;
+		this.#menuList.forEach((_, menu) => {
+			totalPrice += PRICE[menu];
+		});
+		return totalPrice;
+	}
 }
 
+console.log(
+	new MenuList([
+		["아이스크림", 1],
+		["레드와인", 1],
+	]).sumTotalPrice()
+);
 export default MenuList;
