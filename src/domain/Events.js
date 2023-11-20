f4050053cc4e9ad5ac8a33f9005b98f02537427a
MiHye <57440192+kangmihye@users.noreply.github.com>
2023-11-20 21:52:55 +0900

    feat: 크리스마스 할인 이벤트 기능 구현
    - 25일 동안 크리스마스 이벤트 적용
    - 1,000원으로 시작하여 크리스마스가 다가올수록 날마다 할인 금액이 100원씩 증가
    - 25일 이후에는 0원을 반환

--- src/domain/Events.js
+++ src/domain/Events.js
@@ -1,7 +1,21 @@
-const Events = {
+class Events {
+	#date;
+	#day;
+
+	constructor(date) {
+		this.#date = date; //0-4: 일-목,  5-6 :금-토
+		this.#day = new Date(2023, 11, date).getDay();
+	}
+
+	discountXmas() {
+		if (this.#date <= 25) return 1000 + (this.#date - 1) * 100;
+		return 0;
+	}
+
 	canGetFreebie(totalPrice) {
 		if (totalPrice >= 120000) return true;
 		return false;
-	},
-};
+	}
+}
+
 export default Events;
